db38d81b4aa7ee14ed62764d9caec4fe259492f5
Yash8948 <yp364144@gmail.com>
2023-02-13 14:52:00 +0530

    13/02/2023 done from yash

--- src/Componenets/NewsNotification.js
+++ src/Componenets/NewsNotification.js
@@ -1,24 +1,51 @@
 import React, { useState, useEffect } from 'react'
 // import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
-import { FiTrash2 } from 'react-icons/fi';
+// import { FiTrash2 } from 'react-icons/fi';
 import './Notification.css'
 import { Link } from 'react-router-dom';
 
 function NewsNotification() {
-    const handleDeleteAll = () => {
 
-    }
-
-
-    const handleDeleteComment = () => {
-
-    }
-    //    const [icon, setIcon] = useState(AiOutlineLike);
-    // const [like, setLike] = useState(false);
-    // const handletoggle = () => {
-    //     setLike(!like)
-    // };
     const [Data, setData] = useState([]);
+  
+
+    const handleDeleteComment = (id) => {  
+        console.log(id);
+
+                var uid = JSON.parse(localStorage.getItem('user')).data.id
+console.log(uid)
+        
+       
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzM4NTMxMjEsImlzcyI6Ik5ld3NBUFAiLCJleHAiOjE2NzY0NDUxMjEsInN1YiI6Ik5ld3MgQVBQIEF1dGhlbnRpY2F0aW9uIn0.aKZFkV4bqFGOKok5CAX897sqBkERhVF6qiPe2CIYPvw");
+        myHeaders.append("Cookie", "ci_session=12af9107c7cb1f15a290434b44c1be817b862317; csrf_cookie_name=2edd6e5df33b18ac19c9b5bed190f876");
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+
+        var params = {'access_key':5670, 'user_id':uid, 'comment_id':id};
+        var url =new URL("https://news.wrteam.in/Api/delete_comment");
+        // var url =new URL("http://news.thewrteam.in/Api/delete_comment");
+        for (let k in params) {
+            url.searchParams.append(k, params[k])
+        };
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(result =>  {
+                
+                    alert(result.message)
+
+                    
+                
+                
+              })
+            .catch(error => console.log('error', error));
+    }
 
 
     useEffect(() => {
@@ -32,8 +59,8 @@ function NewsNotification() {
             redirect: 'follow'
         };
 
-        // fetch("https://news.wrteam.in/Api/get_notification?access_key=5670&offset=0&limit=10", requestOptions)
-        fetch("http://news.thewrteam.in/Api/get_notification?access_key=5670&offset=0&limit=10", requestOptions)
+        fetch("https://news.wrteam.in/Api/get_notification?access_key=5670&offset=0&limit=10", requestOptions)
+        // fetch("http://news.thewrteam.in/Api/get_notification?access_key=5670&offset=0&limit=10", requestOptions)
             .then(response => response.json())
             .then(result => {
                 setData(result.data)
@@ -47,36 +74,36 @@ function NewsNotification() {
     return (
         <div id="main-Noticard" className='my-3'>
             <div className="d-flex bd-highlight mb-3">
-                <Link to='/Persnol_Notification'id='btnNotification1' className="btn mx-1 bd-highlight" > Personal </Link>
+                <Link to='/Persnol_Notification' id='btnNotification1' className="btn mx-1 bd-highlight" > Personal </Link>
                 <Link to='/News_Notification' id='btnNewsnoti' className="btn mx-1 bd-highlight" > News</Link>
                 {/* <button  id='btnNotification1' className="btn  btn mx-1 ms-auto bd-highlight" onClick={handleDeleteAll} > Delete All</button> */}
             </div>
             <div className='my-3'>
-                {Data.length === 0
+                {/* {Data.length === 0
                     ? "Loading"
-                    : (<>
-                        {Data.map((d, index) => (
+                    : (<> */}
+                        {Data && Data.map((d, index) => (
                             <div className="card my-3" key={index}>
                                 <div className="card-body d-flex bd-highlight" id='card-noti'>
-                                    <img id='noti_img'src={d.image} alt="" />
-                                     {/* <button className="btn bd-highlight" style={{ color: "#EE2934", background: "" }} id='it' onClick={handletoggle}>
+                                    <img id='noti_img' src={d.image} alt="" />
+                                    {/* <button className="btn bd-highlight" style={{ color: "#EE2934", background: "" }} id='it' onClick={handletoggle}>
                                         {like ? <AiFillLike size={35} /> : <AiOutlineLike size={35} />}
                                     </button> */}
                                     <div>
                                         {/* <p className='bd-highlight'>{d.title}</p> */}
-                                        <h4>{d.title.slice(0,100)}...</h4>
-                                        <p className='bd-highlight' > {d.message.slice(0,100)}...</p>
-                                        <p className='bd-highlight' style={{fontSize: "medium", paddingTop: "10px"}}> {d.date_sent}</p>
+                                        <h4>{d.title.slice(0, 100)}...</h4>
+                                        <p className='bd-highlight' > {d.message.slice(0, 100)}...</p>
+                                        <p className='bd-highlight' style={{ fontSize: "medium", paddingTop: "10px" }}> {d.date_sent}</p>
                                     </div>
 
                                     <div className='iconTrash ms-auto bd-highlight'>
-                                    <button className="btn  btn m-2 " id='btntrash' onClick={handleDeleteComment}>Delete</button>
+                                        <button className="btn  btn m-2 " id='btntrash' onClick={()=>handleDeleteComment(d.id)}>Delete</button>
 
                                     </div>
                                 </div>
                             </div>
                         ))}
-                    </>)}
+                    {/* </> )} */}
             </div>
         </div>
     )
--- src/Componenets/SignIn_Modal.js
+++ src/Componenets/SignIn_Modal.js
@@ -105,7 +105,7 @@ function SignIn_Modal(props) {
                 console.log(err.message);
             })
     }
-    
+
     const [phonenum, setPhonenum] = useState(null);
     const auth = getAuth();
     const Signin = () => {
@@ -114,7 +114,8 @@ function SignIn_Modal(props) {
                 // Signed in 
                 const user = userCredential.user;
                 // ...
-                console.log(user.emailVerified);
+                console.log(user);
+
                 if (user.emailVerified) {
                     // alert("Verified")
                     props.setIsLogout(true)
@@ -124,6 +125,48 @@ function SignIn_Modal(props) {
                 }
                 // props.setIsLogout(true)
                 props.onHide()
+
+
+
+        
+
+                var myHeaders = new Headers();
+                myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzM4NTI5MDUsImlzcyI6Ik5ld3NBUFAiLCJleHAiOjE2NzY0NDQ5MDUsInN1YiI6Ik5ld3MgQVBQIEF1dGhlbnRpY2F0aW9uIn0.BR2kPD21Tc-z4Ye7JnS6CvHtNKf3KOR2EsZUdN9ljSo");
+                myHeaders.append("Cookie", "ci_session=12af9107c7cb1f15a290434b44c1be817b862317; csrf_cookie_name=2edd6e5df33b18ac19c9b5bed190f876");
+
+                var requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    redirect: 'follow'
+                };
+
+                fetch("http://news.wrteam.in/Api/generate_token", requestOptions)
+                    .then(response => response.text())
+                    .then(result => {
+
+                        localStorage.setItem('token', result)
+
+                        var formdata2 = new FormData();
+                        formdata2.append("access_key", "5670");
+                        formdata2.append("firebase_id", "2QLzifDNG1aDGah45l6om3C9OSi2");
+                        formdata2.append("email", formValues.email);
+                        formdata2.append("type", "email");
+
+
+                        var requestOptions2 = {
+                            method: 'POST',
+                            headers: myHeaders,
+                            body: formdata2,
+                            redirect: 'follow'
+                        };
+
+                        fetch("https://news.wrteam.in/Api/user_signup", requestOptions2)
+                            .then(response => response.text())
+                            .then(result => localStorage.setItem('user',result))
+                            .catch(error => console.log('error', error));
+
+                    })
+                    .catch(error => console.log('error', error));
             })
             .catch((error) => {
                 const errorCode = error.code;
